d457f1c79450e6fdbe7dd99f56f79c585f82907a
Z-SHOCK_E <sakiwick8@gmail.com>
2025-04-05 03:31:44 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple launch script creator
+- ## What does it do ?
+  - ### It's a simple bash script that creates launch scripts for executables.
---
+++ scriptGen.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+echo "--------LaunchScriptGen--------"
+read -p "Enter path to executable: " executable
+read -p "Application name: " appname
+read -p "Enter launch script name: " name
+
+if [ -x "$executable" ]; then
+   echo "Creating executable launch script......."
+   cat <<EOF > "$name"
+#!/usr/bin/env bash
+echo "Launching $appname........"
+exec "$executable"
+EOF
+   chmod +x "$name"
+   echo "Done :)"
+else
+   echo "Executable not found"
+fi
